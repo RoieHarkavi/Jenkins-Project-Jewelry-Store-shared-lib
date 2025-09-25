13ea0aa1f0e3c53974be2f276907c4771a9ed4a7
Roie <roieadi@gmail.com>
2025-09-25 13:46:19 +0300

    ""

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,15 +1,15 @@
-def call(String dockerImage, String commitHash, String nexusCredentialsId) {
+def call(String dockerImage, String imageTag, String nexusCredentialsId) {
     docker.image('roieharkavi/jewelry-agent3:latest')
           .inside('--user root -v /var/run/docker.sock:/var/run/docker.sock') {
         withCredentials([usernamePassword(credentialsId: nexusCredentialsId,
                                          usernameVariable: 'NEXUS_USER',
                                          passwordVariable: 'NEXUS_PASS')]) {
-            sh '''
-                echo "$NEXUS_PASS" | docker login -u "$NEXUS_USER" --password-stdin nexus:8082
-                docker build -t ${dockerImage}:${commitHash} -t ${dockerImage}:latest .
-                docker push ${dockerImage}:${commitHash}
+            sh """
+                echo "\$NEXUS_PASS" | docker login -u "\$NEXUS_USER" --password-stdin nexus:8082
+                docker build -t ${dockerImage}:${imageTag} -t ${dockerImage}:latest .
+                docker push ${dockerImage}:${imageTag}
                 docker push ${dockerImage}:latest
-            '''
+            """
         }
     }
 }
