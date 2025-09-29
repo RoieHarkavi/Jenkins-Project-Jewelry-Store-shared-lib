f32fd0a2a2384661bfab8dd8fde5b63353b93f05
RoieHarkavi <roieadi@gmail.com>
2025-09-29 20:10:12 +0300

    ""

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -5,7 +5,7 @@ def call(String dockerImage, String imageTag, String nexusCredentialsId, String 
         
         withEnv(["DOCKER_IMAGE=${dockerImage}", "IMAGE_TAG=${imageTag}", "ENVIRONMENT=${environment}"]) {
             sh '''
-                echo "$NEXUS_PASS" | docker login -u "$NEXUS_USER" --password-stdin nexus:8082
+                echo "$NEXUS_PASS" | docker login -u "$NEXUS_USER" --password-stdin http://nexus:8082
                 docker build -t "$DOCKER_IMAGE:$IMAGE_TAG" .
                 docker push "$DOCKER_IMAGE:$IMAGE_TAG"
             '''
