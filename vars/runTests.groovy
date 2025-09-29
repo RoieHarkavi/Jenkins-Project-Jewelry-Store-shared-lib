2f45e9cfd1b1c83a8f8a54111041382a613bb75d
RoieHarkavi <roieadi@gmail.com>
2025-09-29 19:15:23 +0300

    ""

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -5,7 +5,6 @@ def call(String dockerImage, String imageTag) {
         sh """
             docker run --rm ${dockerImage}:${imageTag} bash -c 'pip3 install -r requirements.txt'
             docker run --rm ${dockerImage}:${imageTag} bash -c 'pylint *.py --rcfile=.pylintrc || true'
-            docker run --rm ${dockerImage}:${imageTag} bash -c 'pytest --junitxml results.xml tests/*.py'
         """
 
         junit allowEmptyResults: true, testResults: 'results.xml'
