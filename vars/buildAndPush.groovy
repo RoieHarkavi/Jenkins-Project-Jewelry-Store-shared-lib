4a0822fa969262ff80257d7a50446608693d258d
RoieHarkavi <roieadi@gmail.com>
2025-09-29 20:06:38 +0300

    ""

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -7,9 +7,8 @@ def call(String dockerImage, String imageTag, String nexusCredentialsId, String 
             sh '''
                 echo "$NEXUS_PASS" | docker login -u "$NEXUS_USER" --password-stdin nexus:8082
                 docker build -t "$DOCKER_IMAGE:$IMAGE_TAG" .
-                docker tag "$DOCKER_IMAGE:$IMAGE_TAG" "$DOCKER_IMAGE:latest"
+                docker tag "$DOCKER_IMAGE:$IMAGE_TAG"
                 docker push "$DOCKER_IMAGE:$IMAGE_TAG"
-                docker push "$DOCKER_IMAGE:latest"
             '''
         }
     }
