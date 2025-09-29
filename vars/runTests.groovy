a2e5679642fdc4566104c3f764e03be0914f031a
RoieHarkavi <roieadi@gmail.com>
2025-09-29 19:14:17 +0300

    ""

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,10 +1,13 @@
 def call(String dockerImage, String imageTag) {
     docker.image('roieharkavi/jewelry-agent3:latest')
           .inside('--user root -v /var/run/docker.sock:/var/run/docker.sock') {
-        sh '''
-            docker run --rm ${dockerImage}:${imageTag} \
-            bash -c "pip3 install -r requirements.txt && python3 -m pytest --junitxml results.xml tests/*.py"
-        '''
+
+        sh """
+            docker run --rm ${dockerImage}:${imageTag} bash -c 'pip3 install -r requirements.txt'
+            docker run --rm ${dockerImage}:${imageTag} bash -c 'pylint *.py --rcfile=.pylintrc || true'
+            docker run --rm ${dockerImage}:${imageTag} bash -c 'pytest --junitxml results.xml tests/*.py'
+        """
+
         junit allowEmptyResults: true, testResults: 'results.xml'
     }
 }
